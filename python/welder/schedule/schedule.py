44fff42f59ae4a696b95b9e51243730084e848b4
leiwang1999 <leiwang1999@outlook.com>
2023-10-07 08:55:30 +0000

    split outputs for diff schedule

--- python/welder/schedule/schedule.py
+++ python/welder/schedule/schedule.py
@@ -5,6 +5,7 @@ from tvm import te
 from ..config import Config, Stride
 from ..te_utils import get_compute_ops, seperate_reduce_ops
 from .tir_elementwise import *
+from .te_elementwise import *
 from .te_reduce import *
 from .te_reduce_interthread import *
 from .te_wmma import *
@@ -16,7 +17,12 @@ def schedule(args: List[te.Tensor], config: Config, shared_inputs: List[te.Tenso
         shared_inputs_strides: Dict[te.Tensor, Stride] = {}, shared_outputs = []):
     ops = get_compute_ops(args)
     reduces_ops, _ = seperate_reduce_ops(ops)
-
+    input_args, output_args = [], []
+    for arg in args:
+        if isinstance(arg.op, te.PlaceholderOp):
+            input_args.append(arg)
+        else:
+            output_args.append(arg)
     schedule_on_inner_stage = False
     for tensor in args:
         if isinstance(tensor.op, te.ComputeOp) and tensor.name not in config.schedule_stages:
@@ -24,7 +30,10 @@ def schedule(args: List[te.Tensor], config: Config, shared_inputs: List[te.Tenso
 
     if len(reduces_ops) == 0:
         assert(not schedule_on_inner_stage)
-        template = TIRElementWiseScheduler
+        if len(output_args) > 1:
+            template = TEElementWiseScheduler
+        else:
+            template = TIRElementWiseScheduler
     elif config.use_tc and config.use_cutlass:
         template = TIRCutlassMMAScheduler
     elif config.use_tc and not config.use_cutlass:
